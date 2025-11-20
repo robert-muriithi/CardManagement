dbec9fd156abae3a8f7d02bc805a6bd07c7bbbad
Robert <robert@Roberts-MacBook-Pro.local>
2025-11-20 13:02:36 +0300

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Coop-CardManagement"
+include(":app")
+include(":features")
+include(":core")
+include(":core:network")
+include(":core:database")
+include(":core:domain")
+include(":features:cards")
